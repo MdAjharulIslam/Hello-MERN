f94bbfba671d39672dc3a1c2ff94e05fff580bd5
ajharul <mdajharulislam327@gmail.com>
2025-07-25 17:15:54 +0600

    add loop

---
+++ loop.js
@@ -0,0 +1,38 @@
+
+
+// for loop 
+
+for(let i = 0; i< 5; i++){
+    console.log(i)
+}
+
+
+//while loop
+
+let i = 1
+
+while(i<10){
+    console.log(i*2);
+    i++;
+}
+
+//do while loop
+
+let j = 2;
+
+do{
+    console.log( j+4)
+j++
+}
+while(j<10)
+
+
+    // for each
+
+    let arr = [1,2,3,4,5 ]
+
+    arr.forEach((value, index) => {
+        console.log(`value: ${value} , index: ${index}`)
+    })
+
+    
