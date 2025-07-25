8979ee4c1f0ff983a722297eb0314c203ec89d66
ajharul <mdajharulislam327@gmail.com>
2025-07-25 16:51:57 +0600

    hello code

---
+++ array.js
@@ -0,0 +1,25 @@
+// Array splice 
+
+const array = [1, 2 , 4, 5 , 3]
+
+const arraySplice = array.splice(2,1, 3, 5)
+console.log(arraySplice)
+console.log(array)
+
+//arry join 
+
+const join = array.join('')
+console.log(join)
+
+//array split
+
+const split = join.split(",")
+console.log(split)
+
+// array concat
+
+const array2= [1,2]
+const array3= [3,4]
+
+const concat = array2.concat(array3)
+console.log(concat)
---
+++ object.js
@@ -0,0 +1,63 @@
+//  const person2 = {
+//     name:"ajharul",
+//     age:21
+
+//  }
+
+
+//  function person (name, age){
+//     this.name = name;
+//     this.age = age;
+//  }
+
+//  const person1= new person("kamrul", 17)
+//  console.log(person1)
+//  console.log(person2.name);
+
+
+//  const arr = [1,2,3,4,5];
+// const dubble = arr.reduce((arr)=>{
+//     return arr+=2;
+// }
+// )
+// console.log(arr)
+// console.log(dubble)
+
+// const name = {
+//     name: "ajharul",
+//     age:21
+// }
+
+// const obj = Object.keys(name);
+// const obj2 = Object.values(name)
+
+// console.log(obj)
+// console.log(obj2)
+
+
+//  const  fetchData = async ()=>{
+// try {
+//     const fdata = await fetch('https://www.google.com');
+
+//     const data = await  fdata.json();
+
+//     console.log( "data:" ,data);
+// } catch (error) {
+//     console.error(error)
+// }
+
+//  }
+
+//  localStorage.setItem('name' , 'ajharul')
+//    localStorage.setItem('name', 'kamrul')
+//  localStorage.setItem('name' , 'ajharul')
+//  localStorage.setItem('name', 'kamrul')
+//  const output = localStorage.getItem(name);
+//  console.log(output)
+
+
+// const a="a";
+// const div = "ajhgarul"
+
+// console.log(typeof (div ));
+
