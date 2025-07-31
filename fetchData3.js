13668efb590aa04daed59a1645337fc05274ccc7
ajharul <mdajharulislam327@gmail.com>
2025-07-31 21:19:16 +0600

    practice fetch data in fetchdata3

--- fetchData3.js
+++ fetchData3.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { useEffect } = require('react');
 
 
 axios.get("https://jsonplaceholder.typicode.com/posts")
@@ -29,3 +30,35 @@ axios.post("https://jsonplaceholder.typicode.com/posts", user,{
 .then(res =>{
     console.log(res.data)
 })
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const user1 = {
+    name : "ajharul ",
+    age : 23, 
+    sex: "male"
+}
+ const token1 = "thisistoken"
+ const [loading, setLoading] = useState(false)
+
+const fetchData = async ()=>{
+    try {
+        setLoading(true)
+        const {data} = await axios.post('https://jsonplaceholder.typicode.com/posts', user1)
+        axios.defaults.headers.common['Authorization'] = `${token1}`
+        if(data.success){
+            console.log( "add data successfully", data)
+            setLoading(false)
+        }else{
+            console.data(error.message);
+        }
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+useEffect(()=>{
+fetchData()
+
+},[])
