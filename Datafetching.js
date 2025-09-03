d86e6e4434eccc621ee88f32c2896de4b4a2c480
ajharul <mdajharulislam327@gmail.com>
2025-09-03 15:35:46 +0600

    practice the fetching data

--- Datafetching.js
+++ Datafetching.js
@@ -1,28 +1,30 @@
-const { useEffect } = require("react");
+// const { useEffect } = require("react");
 
-const object = {
-  name: "ajharul",
-  age: 23,
-};
+// const object = {
+//   name: "ajharul",
+//   age: 23,
+// };
 
-const FetchData = async () => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const res = await data.json();
-  res.map((dataa) => {
-    console.log(dataa.body);
-  });
-};
+// const FetchData = async () => {
+//   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   const res = await data.json();
+//   res.map((dataa) => {
+//     console.log(dataa.body);
+//   });
+// };
 
-FetchData();
+// FetchData();
 
 // console.log(object?.name)
-// useEffect(()=>{
+// 
 //    const Data = async ()=>{
+// useEffect(()=>{
 //     const data = await fetch("https://jsonplaceholder.typicode.com/posts",
 //     {
 //      method: "POST" ,
 //      headers:{
 //         "Content-Type": "application/json",
+//         "Authorization": ${token}
 //      },
 //      body: JSON.stringify(object)
 //     }
@@ -34,3 +36,36 @@ FetchData();
 
 // Data();
 // })
+
+
+
+const axios = require("axios");
+// const fetchData = async ()=>{
+//   try {
+//     const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+//     console.log(res.data)
+//   } catch (error) {
+//     console.error(error.message)
+//   }
+// }
+// fetchData()
+
+
+const object = {
+  name: "ajharul",
+  age: 23,
+};
+
+const postData = async ()=>{
+  try {
+    const res = await axios.post("https://jsonplaceholder.typicode.com/posts", object,{
+      headers:{
+        "Content-Type": "application/json"
+      }
+    })
+   console.log(res.data)
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+postData()
